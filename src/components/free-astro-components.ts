324983110f1f95e065f7644adc6776db4be4165b
Kanako Inamine <inakana.oki08@gmail.com>
2025-07-26 17:27:21 +0200

    modal and skill

---
+++ src/components/free-astro-components.ts
@@ -0,0 +1,7 @@
+declare module 'free-astro-components' {
+    export const Button: any;
+    export const Modal: any;
+    export const ModalHeader: any;
+    export const ModalBody: any;
+    export const ModalFooter: any;
+}
---
+++ src/components/projectData.ts
@@ -0,0 +1,29 @@
+export const projectData = [
+    {
+        id: 'modal-01',
+        state: 'finished',
+        title: 'Hospital Incident Report Application for Clinica Sagrada Famili',
+        description: 'This is a real project. Developed a web application using React and Node.js for managing ticket-based reports in a hospital as part of an academic project.',
+        image: {
+            src: '/assets/card/thumb01.jpg',
+            srcset: '/assets/card/thumb01.jpg 1x, /assets/card/thumb01@2x.jpg 2x',
+        },
+        tags: ['react', 'node.js'],
+        links: [
+            { href: 'https://github.com/SF-ClinicProject', icon: 'github-logo' },
+            { href: 'https://project-csf-lp.vercel.app/', icon: 'linkBlank' },
+        ]
+    },
+    {
+        id: 'modal-02',
+        state: 'develop',
+        title: 'Ticket base ...',
+        description: 'Under development project using React and Node.js.',
+        image: {
+            src: 'https://placehold.jp/150x150.png',
+            srcset: 'https://placehold.jp/150x150.png',
+        },
+        tags: ['react', 'node.js'],
+        links: []
+    } 
+];
